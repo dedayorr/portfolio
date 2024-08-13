cc84c27902e0190f69be3b6ca773fccfd2d8f7e3
Elijah Adeyeye <elijahadeyeye@Elijahs-MacBook-Air.local>
2024-08-13 20:03:40 +0100

    first

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Navbar.css"
+
+function Navbar() {
+  return (
+    <div className='bg-[#030712] h-[70px]'>
+        <div className='text-white cursive font-extrabold text-[25px] underline'>Elijah Adeyeye</div>
+        
+    </div>
+  )
+}
+
+export default Navbar
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        textColor: "#D1D5DB",
+        bgColor1: "#030712",
+        bgColor2: "#111827",
+      },
+    },
+  },
+  plugins: [],
+};
