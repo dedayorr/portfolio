9ca87fb378e0307c20a5455e7b21ecbd68909763
Elijah Adeyeye <elijahadeyeye@Elijahs-MacBook-Air.local>
2024-08-14 02:04:03 +0100

    almost

--- src/components/Page/Page.jsx
+++ src/components/Page/Page.jsx
@@ -94,52 +94,281 @@ function Page() {
       </div>
 
       {/* ===============Section Three========  */}
-      <div className="bg-bgColor1 py-[60px] flex flex-col gap-[10px] text-textColor px-[16px]">
+      <div className="bg-bgColor1 pt-[70px] pb-[50%] flex flex-col gap-[40px] text-textColor px-[16px]">
         <p className="mx-auto text-textColor bg-[#374151] w-[105px] h-[28px] rounded-[12px] flex justify-center items-center">
           Skills
         </p>
         <p>The skills, tools and technologies I am good at:</p>
         <div className="grid grid-cols-3 gap-[10%] ">
-          <div className="">
-            <img className="w-[64px] h-[64px]" src="./javascript.png" alt="js" />
+          <div className="items-center flex flex-col justify-center gap-3">
+            <img
+              className="w-[64px] h-[64px]"
+              src="./javascript.png"
+              alt="js"
+            />
             <p>Javascript</p>
           </div>
-          <div className="">
+          <div className="items-center flex flex-col justify-center gap-3">
             <img className="w-[64px] h-[64px]" src="./react.png" alt="js" />
             <p>React</p>
           </div>
-          <div className="">
-            <img className="w-[64px] h-[64px]" src="./typescript.png" alt="js" />
+          <div className="items-center flex flex-col justify-center gap-3">
+            <img
+              className="w-[64px] h-[64px]"
+              src="./typescript.png"
+              alt="js"
+            />
             <p>Typescript</p>
           </div>
-          <div className="">
+          <div className="items-center flex flex-col justify-center gap-3">
             <img className="w-[64px] h-[64px]" src="./nodejs.png" alt="js" />
             <p>Node.js</p>
           </div>
-          <div className="">
+          <div className="items-center flex flex-col justify-center gap-3">
             <img className="w-[64px] h-[64px]" src="./nextjs.png" alt="js" />
             <p>Next.js</p>
           </div>
-          <div className="">
+          <div className="items-center flex flex-col justify-center gap-3">
             <img className="w-[64px] h-[64px]" src="./express.png" alt="js" />
             <p>Express.js</p>
           </div>
-          <div className="">
+          <div className="items-center flex flex-col justify-center gap-3">
             <img className="w-[64px] h-[64px]" src="./mongodb.png" alt="js" />
             <p>MongoDB</p>
           </div>
-          <div className="">
+          <div className="items-center flex flex-col justify-center gap-3">
             <img className="w-[64px] h-[64px]" src="./tailwind.png" alt="js" />
             <p>Tailwindcss</p>
           </div>
-          <div className="">
+          <div className="items-center flex flex-col justify-center gap-3">
             <img className="w-[64px] h-[64px]" src="./git.png" alt="js" />
             <p>Git</p>
           </div>
-          <div className="">
+          <div className="items-center flex flex-col justify-center gap-3">
             <img className="w-[64px] h-[64px]" src="./figma.png" alt="js" />
             <p>Figma</p>
           </div>
+        </div>
+      </div>
+
+      {/* ==========Section Four============= */}
+      <div className="bg-[#111827] py-[60px] text-textColor px-[16px] text-justify flex flex-col gap-[20px]">
+        {" "}
+        <p className="mx-auto text-textColor bg-[#374151] w-[105px] h-[28px] rounded-[12px] flex justify-center items-center">
+          Experience
+        </p>
+        <p className="text-center">Here is a quick summary of my experience</p>
+        <div>
+          <div className="h-[] bg-[#1F2937]  p-5 rounded-[15px] flex flex-col gap-[10px]">
+            <p className="font-bold text-[25px] text-[#14A800]">AremxyPlug</p>
+            <p className="font-thin text-[14px]">
+              June 12 2023 - January 10 2024
+            </p>
+            <p className="font- text-[18px] font-semibold">
+              Front-end Developer
+            </p>
+            <ul className="list-disc flex flex-col gap-[10px]">
+              <li className="text-[14px]">
+                Built a Fintech, Telecom and Digital Service platform to develop
+                and provide innovative products and services that combine
+                financial technology with telecommunications technology
+              </li>
+              <li className="text-[14px]">
+                Design and update layouts to meet usability and performance
+                requirements.
+              </li>
+              <li className="text-[14px]">
+                Collaborate with team to create Functional and Responsive web
+                projects using React.js and Tailwind Css
+              </li>
+              <li className="text-[14px]">
+                Develop and maintain courteous and effective working
+                relationships.
+              </li>
+              <li className="text-[14px]">
+                Paid attention to detail while completing assignments.
+              </li>
+              <li className="text-[14px]">
+                Cultivate interpersonal skills by building positive
+                relationships with others.
+              </li>
+            </ul>
+          </div>
+          <br />
+          <div className="h-[] bg-[#1F2937]  p-5 rounded-[15px] flex flex-col gap-[10px]">
+            <p className="font-bold text-[25px] text-[#14A800]">
+              ITSkillsCenter
+            </p>
+            <p className="font-thin text-[14px]">November 22 2020 - May 2023</p>
+            <p className="font- text-[18px] font-semibold">
+              Software Developer
+            </p>
+            <ul className="list-disc flex flex-col gap-[10px]">
+              <li className="text-[14px]">
+                Designed intuitive graphical user interfaces to improve user
+                experience.
+              </li>
+              <li className="text-[14px]">
+                Designed reusable and reliable code for use within distributed
+                cloud environments.
+              </li>
+              <li className="text-[14px]">
+                Developed responsive software for desktop and mobile view.
+              </li>
+              <li className="text-[14px]">
+                Contributed ideas and suggestions in team meetings and delivered
+                updates on deadlines, designs, and enhancements.
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      {/* =========Section Five=============== */}
+      <div className="bg-bgColor1 pt-[70px] text-textColor px-[16px] flex flex-col gap-[15px]">
+        <p className="mx-auto text-textColor bg-[#374151] w-[105px] h-[28px] rounded-[12px] flex justify-center items-center">
+          Experience
+        </p>
+        <p className="text-center">
+          Some of the noteworthy projects I have built:
+        </p>
+        <div>
+          <div className="bg-[#1F2937]">
+            <div className="bg-[#374151] p-5">
+              <img
+                className="rounded-[15px]"
+                src="./gailymaids.png"
+                alt="project-one"
+              />
+            </div>
+            <div className=" p-5 flex flex-col gap-[18px]">
+              <p className="font-extrabold text-[18px]">Gaily Maids</p>{" "}
+              <p className="font-light text-justify">
+                An all-encompassing cleaning service platform designed to
+                provide exceptional cleaning solutions for residential,
+                commercial, and industrial clients. Our website serves as a
+                one-stop destination for booking professional cleaning services,
+                ensuring a sparkling clean environment for homes, offices, and
+                other spaces.
+              </p>
+              <div className="flex flex-wrap gap-[15px]">
+                {" "}
+                <p className=" text-textColor bg-[#374151] w-[105px] h-[28px] rounded-[12px] flex justify-center items-center">
+                  Next.Js
+                </p>
+                <p className=" text-textColor bg-[#374151] w-[105px] h-[28px] rounded-[12px] flex justify-center items-center">
+                  Tailwindcss
+                </p>
+                <p className=" text-textColor bg-[#374151] w-[105px] h-[28px] rounded-[12px] flex justify-center items-center">
+                  EmailJs
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <br/>
+          <div className="bg-[#1F2937]">
+            <div className="bg-[#374151] p-5">
+              <img
+                className="rounded-[15px]"
+                src="./gailymaids.png"
+                alt="project-one"
+              />
+            </div>
+            <div className=" p-5 flex flex-col gap-[18px]">
+              <p className="font-extrabold text-[18px]">Gaily Maids</p>{" "}
+              <p className="font-light text-justify">
+                An all-encompassing cleaning service platform designed to
+                provide exceptional cleaning solutions for residential,
+                commercial, and industrial clients. Our website serves as a
+                one-stop destination for booking professional cleaning services,
+                ensuring a sparkling clean environment for homes, offices, and
+                other spaces.
+              </p>
+              <div className="flex flex-wrap gap-[15px]">
+                {" "}
+                <p className=" text-textColor bg-[#374151] w-[105px] h-[28px] rounded-[12px] flex justify-center items-center">
+                  Next.Js
+                </p>
+                <p className=" text-textColor bg-[#374151] w-[105px] h-[28px] rounded-[12px] flex justify-center items-center">
+                  Tailwindcss
+                </p>
+                <p className=" text-textColor bg-[#374151] w-[105px] h-[28px] rounded-[12px] flex justify-center items-center">
+                  EmailJs
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <br/>
+          <div className="bg-[#1F2937]">
+            <div className="bg-[#374151] p-5">
+              <img
+                className="rounded-[15px]"
+                src="./gailymaids.png"
+                alt="project-one"
+              />
+            </div>
+            <div className=" p-5 flex flex-col gap-[18px]">
+              <p className="font-extrabold text-[18px]">Gaily Maids</p>{" "}
+              <p className="font-light text-justify">
+                An all-encompassing cleaning service platform designed to
+                provide exceptional cleaning solutions for residential,
+                commercial, and industrial clients. Our website serves as a
+                one-stop destination for booking professional cleaning services,
+                ensuring a sparkling clean environment for homes, offices, and
+                other spaces.
+              </p>
+              <div className="flex flex-wrap gap-[15px]">
+                {" "}
+                <p className=" text-textColor bg-[#374151] w-[105px] h-[28px] rounded-[12px] flex justify-center items-center">
+                  Next.Js
+                </p>
+                <p className=" text-textColor bg-[#374151] w-[105px] h-[28px] rounded-[12px] flex justify-center items-center">
+                  Tailwindcss
+                </p>
+                <p className=" text-textColor bg-[#374151] w-[105px] h-[28px] rounded-[12px] flex justify-center items-center">
+                  EmailJs
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <br/>
+          <div className="bg-[#1F2937]">
+            <div className="bg-[#374151] p-5">
+              <img
+                className="rounded-[15px]"
+                src="./gailymaids.png"
+                alt="project-one"
+              />
+            </div>
+            <div className=" p-5 flex flex-col gap-[18px]">
+              <p className="font-extrabold text-[18px]">Gaily Maids</p>{" "}
+              <p className="font-light text-justify">
+                An all-encompassing cleaning service platform designed to
+                provide exceptional cleaning solutions for residential,
+                commercial, and industrial clients. Our website serves as a
+                one-stop destination for booking professional cleaning services,
+                ensuring a sparkling clean environment for homes, offices, and
+                other spaces.
+              </p>
+              <div className="flex flex-wrap gap-[15px]">
+                {" "}
+                <p className=" text-textColor bg-[#374151] w-[105px] h-[28px] rounded-[12px] flex justify-center items-center">
+                  Next.Js
+                </p>
+                <p className=" text-textColor bg-[#374151] w-[105px] h-[28px] rounded-[12px] flex justify-center items-center">
+                  Tailwindcss
+                </p>
+                <p className=" text-textColor bg-[#374151] w-[105px] h-[28px] rounded-[12px] flex justify-center items-center">
+                  EmailJs
+                </p>
+              </div>
+            </div>
+          </div>
+      
+
         </div>
       </div>
     </div>
